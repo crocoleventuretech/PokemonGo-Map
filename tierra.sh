00bef557a7cae257e3e025b4bb713b1303396d91
crocole <crocole@venturetech.net>
2016-07-23 09:04:59 -0500

    Update port number

--- tierra.sh
+++ tierra.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-python runserver.py -a google -u connerrocolepokemongo -p pokemongomap -l "40.765782, -96.679083" -st 7
+python runserver.py -a google -u connerrocolepokemongo -p pokemongomap -l "40.765782, -96.679083" -st 7 -P 5100
